64c66647604800180664531f0116e2780fc44e10
Jason Fletcher <jtmfletcher@gmail.com>
2023-04-23 16:08:05 -0400

    added player movement

--- examples/final-project-game.py
+++ examples/final-project-game.py
@@ -9,6 +9,7 @@ from gpiozero import Button
 
 from unicornhatmini import UnicornHATMini
 uh = UnicornHATMini()
+width, height = uh.get_shape()
 uh.set_brightness(0.3)
 
 class JumpingGame():
@@ -23,12 +24,15 @@ class JumpingGame():
     def player_up(self):
         if self.pos_y > 0:
             self.pos_y -= 1
+            uh.set_pixel(self.pos_x, (self.pos_y + 1), 0, 0, 0)
+      
+            
+    def player_down(self):
+        if self.pos_y <= height - self.size:
+            self.pos_y += 1
+            uh.set_pixel(self.pos_x, (self.pos_y - 1), 0, 0, 0)
             
     def update(self):
-        
-        
-        
-        
         uh.set_pixel(self.pos_x, self.pos_y, 255, 255, 255)
         uh.show()
         
@@ -37,6 +41,12 @@ class JumpingGame():
 
 
 game = JumpingGame()
+
+button_a = Button(5)
+button_b = Button(6)
+
+button_a.when_pressed = game.player_up
+button_b.when_pressed = game.player_down
 
 while game.game_on:
     game.update()
