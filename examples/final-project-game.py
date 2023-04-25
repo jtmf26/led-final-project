635225b3ce6f3ef9cef96da816bbfa2d6260e82e
Jason Fletcher <jtmfletcher@gmail.com>
2023-04-25 18:37:03 -0400

    Added Collision to obstacle and scrolling spawn

--- examples/final-project-game.py
+++ examples/final-project-game.py
@@ -13,7 +13,8 @@ uh = UnicornHATMini()
 width, height = uh.get_shape()
 uh.set_brightness(0.5)
 
-class game_player():
+
+class game():
     def __init__(self):
         self.colors = (
             (255, 0, 0),
@@ -24,7 +25,7 @@ class game_player():
             (0, 0, 255),
             (145, 13, 212),
             (247, 37, 202))
-        self.game_on= True
+        self.game_on = True
         self.pos_x = 4
         self.pos_y = 4
         self.size = 2
@@ -45,42 +46,76 @@ class game_player():
  
     def color_picker(self):
         self.color = random.choice(self.colors)
-            
+        
             
             
     def update(self):
         uh.set_pixel(self.pos_x, self.pos_y, *self.color)
         uh.show()
+        
+    def game_over(self):
+        self.game_on = False
+        for x in range(16):
+            for y in range(6):
+                uh.set_pixel(x, y, 255, 0, 0)
+        
+    def game_restart(self):
+        self.game_on == True
 
+       
+    def get_pos_x(self):
+            return self.pos_x
+            
+    def get_pos_y(self):
+            return self.pos_y
+        
         
 
 
 class obstacles():
     def __init__(self):
-        self.pos_x = 10
+        self.pos_x = 16
         self.pos_y = 4
         self.size = 3
+        self.delay = 0.1
+
         
     def update(self):
-        for x in range(width):
-            for y in range(self.size):
-                if self.pos_x > 0:
-                    self.pos_x - 1
-                    uh.set_pixel(self.pos_x, self.pos_y + y, 42, 26, 14)
-           
-     
-            
-    
-   
-            
-           
-           
+        pos_x_last = self.pos_x
+        pos_y_last = self.pos_y
+        if self.pos_x > 0:
+            self.pos_x = self.pos_x - 1
+            for i in range(self.size):
+                uh.set_pixel(self.pos_x, self.pos_y + i, 42, 26, 14)
+                time.sleep(self.delay)
+                uh.set_pixel(pos_x_last, self.pos_y + i, 0, 0, 0)
+                
+        elif self.pos_x == 0:
+            if self.delay > 0.03:
+                self.delay -= 0.01
+            uh.clear()
+            self.pos_x = 16
+            self.pos_y = random.randint(0, 3)
+            for i in range(self.size):
+                uh.set_pixel(self.pos_x, self.pos_y + i, 42, 26, 14)
+                time.sleep(self.delay)
+                uh.set_pixel(pos_x_last, self.pos_y + i, 0, 0, 0)
                 
                 
+    def get_pos_y(self):
+        return [i + self.pos_y for i in range(self.size)]
+      
+    def get_pos_x(self):
+        return self.pos_x
+      
+                
+                
+                
+                    
+game = game()
+obstacle = obstacles() 
 
 
-game = game_player()
-obstacle = obstacles()
 
 button_a = Button(5)
 button_b = Button(6)
@@ -90,8 +125,21 @@ button_y = Button(24)
 button_a.when_pressed = game.player_up
 button_b.when_pressed = game.player_down
 button_x.when_pressed = game.color_picker
+button_y.when_pressed = game.game_restart
+
+
+    
 
 while game.game_on:
     game.update()
     obstacle.update()
-    
+    obstacle.get_pos_x()
+    obstacle.get_pos_y()
+    game.get_pos_x()
+    game.get_pos_y()
+    if game.get_pos_y() in obstacle.get_pos_y() and game.get_pos_x() == obstacle.get_pos_x():
+        game.game_over()
+        
+        
+
+
